63129aa45e8167ccb8d7edf6d541834af3f2d262
flaskdz <496487991@qq.com>
2017-03-15 14:14:47 +0800

    'change'

--- manage.py
+++ manage.py
@@ -3,68 +3,70 @@
 from app import create_app, db
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User, Role, Post
-import sys
-import os
+from app.models import User, Role, Post, Permission, Comment
 
+
+import os
 COV = None
 if os.environ.get('FLASK_COVERAGE'):
-	import coverage
-	COV = coverage.coverage(branch= True, include= 'app/*')
-	COV.start()
-	
+    import coverage
+    COV = coverage.coverage(branch= True, include= 'app/*')
+    COV.start()
+    
 
-#support chinese
+#支持中文
+import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-app = create_app('default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
 
 @manager.command
 def test(coverage = False):
-	"""Run the unit tests"""
-	if coverage and not os.environ.get('FLASK_COVERAGE'):
-		import sys
-		os.environ['FLASK_COVERAGE'] = '1'
-		os.execvp(sys.executable, [sys.executable] + sys.argv)
-	import unittest
-	tests = unittest.TestLoader().discover('tests')
-	unittest.TextTestRunner(verbosity=2).run(tests)
-	if COV:
-		COV.stop()
-		COV.save()
-		print('Coverage Summary')
-		COV.report()
-		basedir = os.path.abspath(os.path.dirname(__file__))
-		covdir = os.path.join(basedir, 'tmp/coverage')
-		COV.html_report(directory=covdir)
-		print('HTML version: file://%s/index.html' % covdir)
-		COV.erase()
+    """Run the unit tests"""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 
 @manager.command
 def deploy():
-	"""Run deployment tasks"""
-	from flask_migrate import upgrade
-	from app.models import Role, User
-	
-	#把数据库迁移到最新修订版本
-	upgrade()
-	
-	#创建用户角色
-	Role.insert_roles()
-	
-	#让所有用户都关注此用户
-	User.add_self_follows()
+    """Run deployment tasks"""
+    from flask_migrate import upgrade
+    from app.models import Role, User
+    
+    #把数据库迁移到最新修订版本
+    upgrade()
+    
+    #创建用户角色
+    Role.insert_roles()
+    
+    #自关注
+    User.add_self_follows()
 
 def make_shell_context():
-	return dict(app = app, db = db, User = User, Role = Role, Post = Post)
+    return dict(app = app, db = db, User = User, Role = Role,
+                Post = Post, Permission = Permission, Comment = Comment)
 
 manager.add_command('shell', Shell(make_context = make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-	manager.run()
+    manager.run()
